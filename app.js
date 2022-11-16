88f01f2a7e5309f34446c94a4f194c70199210a4
Padillaguadalupe <Padillaguadalupe.epet20@gmail.com>
2022-11-16 09:44:12 -0300

    primer prudcto mal no leer gracias

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 const stockProductos = [
+    {
+        id: 1,
+        nombre: "Crash Bandicoot",
+        cantidad: 1,
+        desc: "Juego plataformero, niveles dificiles",
+        precio: 1200,
+        img: "img/Crash.jpg",
+      },
