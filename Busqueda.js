9051488191574fa30192719e5755ca656fa06808
Padillaguadalupe <Padillaguadalupe.epet20@gmail.com>
2022-11-16 11:32:24 -0300

    condicional if

--- Busqueda.js
+++ Busqueda.js
@@ -7,3 +7,15 @@ function showSearch(){
 
 let btn =document.getElementById("btn");
 let click = false;
+
+btn.addEventListener("click", () => {
+    if (!click){
+        click= true;
+        btn.innerHTML = `<img src="IMAGENES/cookieFav2.png">`;
+
+    }else{
+        click  = false;
+        btn.innerHTML = `<img src="IMAGENES/cookieFav3.png">`;
+
+    }
+})
