f305f41212e2970b682ea888bdc9cccf0f3efbbf
Padillaguadalupe <Padillaguadalupe.epet20@gmail.com>
2022-11-16 10:00:53 -0300

    comiteando el segundo producto

--- app.js
+++ app.js
@@ -8,3 +8,11 @@ const stockProductos = [
         precio: 50,
         img: "IMAGENES/cookieSim2.jpg",
       },
+      {
+        id: 2,
+        nombre: "Amancay",
+        cantidad: 1,
+        desc: "Cookie deliciosa con semillas integrales",
+        precio: 50,
+        img: "IMAGENES/cookiesSim3.jpg",
+      },
