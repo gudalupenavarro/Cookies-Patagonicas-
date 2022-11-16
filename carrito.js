32bddf26010b9f0b4678c484482da5c82b64cc1f
Padillaguadalupe <Padillaguadalupe.epet20@gmail.com>
2022-11-16 11:41:21 -0300

    carrito forEach

--- carrito.js
+++ carrito.js
@@ -1,1 +1,6 @@
 export function procesarPedido() {
+    carrito.forEach((prod) => {
+        const contenedorCompra = document.querySelector('#contenedorCompra')
+        const { id, nombre, precio, desc, img, cantidad } = prod;
+        const div = document.createElement("div");
+        div.innerHTML += `
